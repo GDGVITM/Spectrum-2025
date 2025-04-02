f7d98dcd182dc9aeb29f6cdc3dca72adb42d1d4e
TanishqChavan10 <tan20051007@gmail.com>
2025-04-03 01:04:18 +0530

    conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import './App.css'
 import Event from './Events/main-event'
 import InfiniteCarousel from './Our-Team/Infinite-Carousel'
+
+
 function App() {
 
   return (
