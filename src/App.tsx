c1651bec9320b029f3c9533ad4d8277c0e51b650
TanishqChavan10 <tan20051007@gmail.com>
2025-04-03 00:41:37 +0530

    Team carousel updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import Event from './Events/main-event'
-
+import InfiniteCarousel from './Our-Team/Infinite-Carousel'
 
 function App() {
 
   return (
     <>
-    <Event/>
+    <InfiniteCarousel/>
     </>
   )
 }
