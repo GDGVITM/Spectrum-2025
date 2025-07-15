a1c8d9bc211e65392525c65d4d035bb107256a62
Shweta <143203479+shwet46@users.noreply.github.com>
2025-07-15 17:30:01 +0530

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import UnderProgress from './UnderProgress';
 import Our_Team from './Our-Team/main-our-team';
 import './index.css';
 import About from './About/About';
